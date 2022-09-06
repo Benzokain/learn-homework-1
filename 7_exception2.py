0989f2a4d59de9508516e9e135261fd1749c8d3b
Yury <night1ps@gmail.com>
2022-09-06 17:25:51 +0300

    fix 7

--- 7_exception2.py
+++ 7_exception2.py
@@ -21,15 +21,15 @@ def discounted(price, discount, max_discount=20):
       price = float(price)
       discount = float(discount)
       max_discount = int(max_discount)
+      if max_discount >= 100:
+          raise ValueError('Слишком большая максимальная скидка')
+      if discount >= max_discount:
+          return price
+      else:
+          return price - (price * discount / 100)
     except (ValueError,TypeError):
       print('Необходимо вводить только числа')
-    if max_discount >= 100:
-        raise ValueError('Слишком большая максимальная скидка')
-    if discount >= max_discount:
-        return price
-    else:
-        return price - (price * discount / 100)
-    
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
