cd3d464fe89d78372aca5d38c221444e72e1a82b
Сафонов Юрий Юрьевич <ysafonov@t1-consulting.ru>
2022-09-07 22:52:19 +0300

    getattrFix

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -43,36 +43,17 @@ def greet_user(update, context):
 def get_date():
     return str(datetime.now().date()).replace('-','/')
 
-# def get_planetlist ():
-#     list_planets = []
-#     for i in ephem._libastro.builtin_planets():
-#         list_planets.append(i[-1])
-#     return list_planets
-
-
-# def talk_to_me(update, context):
-#     user_text = update.message.text
-#     print(user_text)
-#     update.message.reply_text(user_text)
-
 def constellation_info(update, context):
     user_text = update.message.text.split()[-1]
     print(user_text)
-    print(f'Планета {user_text} в созвездии:')
-    program = f'update.message.reply_text(ephem.constellation(ephem.{user_text}(get_date())))'
-    exec(program)
-    # update.message.reply_text(ephem.constellation(ephem.Mars(get_date())))
+    result = getattr(ephem, user_text)(get_date())
+    update.message.reply_text(f'{user_text} в созвездии {ephem.constellation(result)}')
+
 
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
-
-# def constellation_info(update, context):
-#     # print(f'Вызван {update.message.text}')
-#     planet = update.message.text.split()[-1]
-#     print(planet)
-
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
